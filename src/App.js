488e014e6c7b68842d46e2f4fe457c45bc5584d8
Pedro Lucas Pereira Flores <pedro.flores@VIARONDON.LOCAL>
2024-05-03 17:44:34 -0300

    Start project + MUI

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+
+import Home from './pages/Home';
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar/>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
